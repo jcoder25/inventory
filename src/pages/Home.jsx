dd40368f5b6b79284acd4975552f7d898ec85fa3
Jashan Jindal <jashanjindal25@gmail.com>
2026-05-10 19:04:23 +0530

    fixed Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,90 +2,11 @@ import { Link } from "react-router-dom";
 
 export default function Home({ inventory }){
 
-const groupedSizes = {};
-
-inventory.forEach(item=>{
-
-const size = item.size;
-
-if(!groupedSizes[size]){
-groupedSizes[size] = 0;
-}
-
-groupedSizes[size] += Number(item.balance || 0);
-
-});
-
-
-if(inventory.length === 0){
-
 return(
 
 <div className="card">
 
-<h2>No Inventory Data Found</h2>
-
-</div>
-
-)
-
-}
-
-
-const sizeData = Object.entries(groupedSizes);
-
-return(
-
-<div className="card">
-
-<h2 style={{marginBottom:"24px"}}>
-Size Stock Summary
-</h2>
-
-<table>
-
-<thead>
-<tr>
-
-<th>Sr No</th>
-<th>Size</th>
-<th>Total Balance</th>
-
-</tr>
-</thead>
-
-<tbody>
-
-{sizeData.map(([size,balance],index)=>(
-
-<tr key={size}>
-
-<td>{index + 1}</td>
-
-<td>
-
-<Link
-to={`/size/${size}`}
-style={{
-color:"#0b2b63",
-fontWeight:"bold",
-textDecoration:"none"
-}}
->
-{size}
-</Link>
-
-</td>
-
-<td>{balance}</td>
-
-</tr>
-
-))}
-
-</tbody>
-
-</table>
+<h1>HOME WORKING</h1>
 
 </div>
 
