320f6afdaf81631e9e066ec5e0f67125e21d61b1
Jashan Jindal <jashanjindal25@gmail.com>
2026-05-10 16:51:16 +0530

    fixed mobile responsive filters

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,43 @@
-
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Sidebar(){
 
+const [open,setOpen] = useState(false);
+
 return(
-<div className="sidebar">
+
+<>
+
+<div className="mobile-topbar">
 
 <h1>FactoryStock</h1>
 
+<button
+className="hamburger"
+onClick={()=>setOpen(!open)}
+>
+☰
+</button>
+
+</div>
+
+<div className={`sidebar ${open ? "show-sidebar" : ""}`}>
+
 <Link to="/">Dashboard</Link>
+
 <Link to="/inventory">Inventory</Link>
+
 <Link to="/materials">Materials</Link>
+
 <Link to="/activity">Activity</Link>
+
 <Link to="/settings">Settings</Link>
 
 </div>
+
+</>
+
 )
+
 }
